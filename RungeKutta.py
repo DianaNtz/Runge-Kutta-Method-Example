bed44c4da7c01d5c7455f26bc46ca6955e1caffa
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-07-30 15:21:49 +0200

    added third order method in RungeKutta.py

--- RungeKutta.py
+++ RungeKutta.py
@@ -15,6 +15,8 @@ t=np.empty(steps+1, dtype='double')
 tn=t0
 u2=np.empty(steps+1, dtype='double')
 un2=u0
+u3=np.empty(steps+1, dtype='double')
+un3=u0
 for i in range(0,steps+1):    
     t[i]=tn
     #Runge Kutta second
@@ -22,11 +24,18 @@ for i in range(0,steps+1):
     k1=dt*f(tn,un2)
     k2=dt*f(tn+0.5*dt,un2+0.5*k1)
     un2=un2+k2
+    #Runge Kutta third
+    u3[i]=un3
+    k1=dt*f(tn,un3)
+    k2=dt*f(tn+0.5*dt,un3+0.5*k1)
+    k3=dt*f(tn+dt,un3-k1+2*k2)
+    un3=un3+k2*(4/6)+k1*(1/6)+k3*(1/6)
     tn=tn+dt
 ua=u0*np.exp(-0.5*(t-a*2)*t)
 ax1 = plt.subplots(1, sharex=True, figsize=(10,5))          
 plt.plot(t,ua,color='black',linestyle='-',linewidth=3,label="$u_a(t)$")
 plt.plot(t,u2,color='yellow',linestyle='-.',linewidth=3,label="$u_2(t)$")
+plt.plot(t,u3,color='lime',linestyle='-.',linewidth=3,label = "$u_3(t)$")
 plt.xlabel("t",fontsize=19) 
 plt.ylabel(r' ',fontsize=19,labelpad=20).set_rotation(0)
 plt.ylim([0,8])
