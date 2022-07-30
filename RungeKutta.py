80259d91d19e40a8990e0a019bfdb0211319a062
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-07-30 15:17:22 +0200

    plotted analytical vs numerical solution of second order Runge Kutta method in RungeKutta.py

--- RungeKutta.py
+++ RungeKutta.py
@@ -23,3 +23,15 @@ for i in range(0,steps+1):
     k2=dt*f(tn+0.5*dt,un2+0.5*k1)
     un2=un2+k2
     tn=tn+dt
+ua=u0*np.exp(-0.5*(t-a*2)*t)
+ax1 = plt.subplots(1, sharex=True, figsize=(10,5))          
+plt.plot(t,ua,color='black',linestyle='-',linewidth=3,label="$u_a(t)$")
+plt.plot(t,u2,color='yellow',linestyle='-.',linewidth=3,label="$u_2(t)$")
+plt.xlabel("t",fontsize=19) 
+plt.ylabel(r' ',fontsize=19,labelpad=20).set_rotation(0)
+plt.ylim([0,8])
+plt.xlim([t0,tfinal]) 
+plt.xticks(fontsize= 17)
+plt.yticks(fontsize= 17) 
+plt.legend(loc=2,fontsize=19,handlelength=3) 
+plt.show()
