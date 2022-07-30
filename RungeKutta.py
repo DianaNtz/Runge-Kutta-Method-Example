5e265909fbfc9ee4697d0f7e8a1c9eb27f5bcd0d
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-07-30 15:24:56 +0200

    added fourth order method in RungeKutta.py

--- RungeKutta.py
+++ RungeKutta.py
@@ -17,6 +17,8 @@ u2=np.empty(steps+1, dtype='double')
 un2=u0
 u3=np.empty(steps+1, dtype='double')
 un3=u0
+u4=np.empty(steps+1, dtype='double')
+un4=u0
 for i in range(0,steps+1):    
     t[i]=tn
     #Runge Kutta second
@@ -30,12 +32,20 @@ for i in range(0,steps+1):
     k2=dt*f(tn+0.5*dt,un3+0.5*k1)
     k3=dt*f(tn+dt,un3-k1+2*k2)
     un3=un3+k2*(4/6)+k1*(1/6)+k3*(1/6)
+    #Runge Kutta fourth
+    u4[i]=un4
+    k1=dt*f(tn,un4)
+    k2=dt*f(tn+0.5*dt,un4+0.5*k1)
+    k3=dt*f(tn+0.5*dt,un4+0.5*k2)
+    k4=dt*f(tn+dt,un4+k3)
+    un4=un4+k2*(2/6)+k1*(1/6)+k3*(2/6)+k4*(1/6)
     tn=tn+dt
 ua=u0*np.exp(-0.5*(t-a*2)*t)
 ax1 = plt.subplots(1, sharex=True, figsize=(10,5))          
 plt.plot(t,ua,color='black',linestyle='-',linewidth=3,label="$u_a(t)$")
 plt.plot(t,u2,color='yellow',linestyle='-.',linewidth=3,label="$u_2(t)$")
 plt.plot(t,u3,color='lime',linestyle='-.',linewidth=3,label = "$u_3(t)$")
+plt.plot(t,u4,color='deepskyblue',linestyle='-.',linewidth=3,label = "$u_4(t)$")
 plt.xlabel("t",fontsize=19) 
 plt.ylabel(r' ',fontsize=19,labelpad=20).set_rotation(0)
 plt.ylim([0,8])
